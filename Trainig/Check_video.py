414ee8ed0dbfbd2d6c801db1ce16ce7081498bf6
dhc518 <dhc000518@gmail.com>
2023-03-15 11:07:58 +0900

    update
    update

--- Trainig/Check_video.py
+++ Trainig/Check_video.py
@@ -8,13 +8,14 @@ LEFT_EYE = [362,382,381,380,374,373,390,249,263,466,388,387,386,385,384,398]
 RIGHT_EYE = [ 33,7,163,144,145,153,154,155,133,173,157,158,159,160,161,246 ]
 LEFT_IRIS = [474,475,476,477 ]
 RIGHT_IRIS = [469,470,471,472 ]
+FACE_HEAD_POSE_LACNMARKS = [1, 33, 61, 199, 291, 263]
 
 capture = cv.VideoCapture("sample_vid.mp4")
 
 with mp_face_mesh.FaceMesh(max_num_faces =1,
                            refine_landmarks =True,
-                           min_detection_confidence =0.5,
-                           min_tracking_confidence =0.5
+                           min_detection_confidence =0.5, # 높이면 정확성 up, 속도 down
+                           min_tracking_confidence =0.5   # 내리면 속도 up, 정학성 down
 ) as face_mesh:
     while True:
         if capture.get(cv.CAP_PROP_POS_FRAMES) == capture.get(cv.CAP_PROP_FRAME_COUNT):
@@ -27,6 +28,13 @@ with mp_face_mesh.FaceMesh(max_num_faces =1,
         if results.multi_face_landmarks:
             mesh_points = np.array([np.multiply([p.x, p.y], [img_w, img_h]).astype(int)
                                  for p in results.multi_face_landmarks[0].landmark])
+            #face_mesh 전부 표시하기 색(B,G,R)
+            for pt in mesh_points:
+                #cv.circle(img, center,radius,color,thicknex,lineType)
+                cv.circle(frame, pt, 1, (255,255,255), -1, cv.LINE_AA)
+
+
+
             print(mesh_points)
             cv.polylines(frame, [mesh_points[LEFT_EYE]], True, (0, 255, 0), 2, cv.LINE_AA)
             cv.polylines(frame, [mesh_points[RIGHT_EYE]], True, (0, 255, 0), 2, cv.LINE_AA)
@@ -38,6 +46,47 @@ with mp_face_mesh.FaceMesh(max_num_faces =1,
             center_right = np.array([r_cx, r_cy], dtype=np.int32)
             cv.circle(frame, center_left, int(l_radius), (0, 0, 255), 2, cv.LINE_AA)
             cv.circle(frame, center_right, int(r_radius), (0, 0, 255), 2, cv.LINE_AA)
+
+            # face_direction
+            face_2d = []
+            face_3d = []
+
+            for idx, lm in enumerate(results.multi_face_landmarks[0].landmark):
+                if idx in FACE_HEAD_POSE_LACNMARKS:
+                    if idx == 1:
+                        nose_2d = (lm.x * img_w, lm.y * img_h)
+                        nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
+                    x, y = int(lm.x * img_w), int(lm.y * img_h)
+
+                    face_2d.append([x, y])
+                    face_3d.append([x, y, lm.z])
+
+            face_2d = np.array(face_2d, dtype=np.float64)
+            face_3d = np.array(face_3d, dtype=np.float64)
+
+            focal_len = 1 * img_w
+
+            camera_mat = np.array([[focal_len, 0, img_h / 2],
+                                   [0, focal_len, img_w / 2],
+                                   [0, 0, 1]])
+
+            dist_mat = np.zeros((4, 1), dtype=np.float64)
+
+            success, rot_vec, trans_vec = cv.solvePnP(face_3d, face_2d, camera_mat, dist_mat)
+
+            rot_mat, jac = cv.Rodrigues(rot_vec)
+            angles, mtxR, mtxQ, Qx, Qy, Qz = cv.RQDecomp3x3(rot_mat)
+            x = angles[0] * 360
+            y = angles[0] * 360
+            z = angles[0] * 360
+
+            # nose+3d_projection, jacobian = cv.projectionPoints(nose_3d, rot_vec, trans_vec, camera_mat, dist_mat)
+
+            p1 = (int(nose_2d[0]), int(nose_2d[1]))
+            p2 = (int(nose_2d[0] + y * 10), int(nose_2d[1] - x * 10))
+
+            cv.line(frame, p1, p2, (255, 255, 0), 3)
+
 
         cv.imshow('Main', frame)
 
--- Trainig/Face_Direction.py
+++ Trainig/Face_Direction.py
@@ -2,6 +2,11 @@ import cv2 as cv
 import numpy as np
 import mediapipe as mp
 
+
+
+
+
+
 #카메라 회전 함수
 def Rotate(src, degrees):
     if degrees == 90:
@@ -46,6 +51,7 @@ LEFT_EYE = [362,382,381,380,374,373,390,249,263,466,388,387,386,385,384,398]
 RIGHT_EYE = [ 33,7,163,144,145,153,154,155,133,173,157,158,159,160,161,246 ]
 LEFT_IRIS = [474,475,476,477 ]
 RIGHT_IRIS = [469,470,471,472 ]
+FACE_HEAD_POSE_LACNMARKS = [1, 33, 61, 199, 291, 263]
 
 
 
@@ -54,8 +60,8 @@ RIGHT_IRIS = [469,470,471,472 ]
 
 with mp_face_mesh.FaceMesh(max_num_faces =1,
                            refine_landmarks =True,
-                           min_detection_confidence =0.5, # 높이면 정확성 up, 속도 down
-                           min_tracking_confidence =0.5   # 내리면 속도 up, 정학성 down
+                           min_detection_confidence =0.5,
+                           min_tracking_confidence =0.5
 ) as face_mesh:
     while True:
         if cam.get(cv.CAP_PROP_POS_FRAMES) == cam.get(cv.CAP_PROP_FRAME_COUNT):
@@ -79,6 +85,47 @@ with mp_face_mesh.FaceMesh(max_num_faces =1,
             center_right = np.array([r_cx, r_cy], dtype=np.int32)
             cv.circle(frame, center_left, int(l_radius), (0, 0, 255), 2, cv.LINE_AA)
             cv.circle(frame, center_right, int(r_radius), (0, 0, 255), 2, cv.LINE_AA)
+
+            # face_direction
+            face_2d = []
+            face_3d = []
+
+            for idx, lm in enumerate(results.multi_face_landmarks[0].landmark):
+                if idx in FACE_HEAD_POSE_LACNMARKS:
+                    if idx == 1:
+                        nose_2d = (lm.x*img_w,lm.y*img_h)
+                        nose_3d = (lm.x*img_w,lm.y*img_h, lm.z*3000)
+                    x,y = int(lm.x * img_w), int(lm.y * img_h)
+
+                    face_2d.append([x,y])
+                    face_3d.append([x, y,lm.z])
+
+            face_2d = np.array(face_2d, dtype=np.float64)
+            face_3d = np.array(face_3d, dtype=np.float64)
+
+            focal_len = 1 * img_w
+
+            camera_mat = np.array([[focal_len, 0, img_h/2],
+                                  [0, focal_len, img_w/2],
+                                  [0,0,1]])
+
+            dist_mat = np.zeros((4,1), dtype = np.float64)
+
+            success, rot_vec, trans_vec = cv.solvePnP(face_3d, face_2d, camera_mat, dist_mat)
+
+            rot_mat, jac = cv.Rodrigues(rot_vec)
+            angles, mtxR, mtxQ, Qx, Qy, Qz = cv.RQDecomp3x3(rot_mat)
+            x = angles[0] * 360
+            y = angles[0] * 360
+            z = angles[0] * 360
+
+            #nose+3d_projection, jacobian = cv.projectionPoints(nose_3d, rot_vec, trans_vec, camera_mat, dist_mat)
+
+            p1 = (int(nose_2d[0]), int(nose_2d[1]))
+            p2 = (int(nose_2d[0]+y*10), int(nose_2d[1] -x * 10))
+
+            cv.line(frame,p1,p2,(255,255,0),3)
+
 
 
         # 이미지를 회전시켜서 img로 돌려받음
